02a28def40b025eb27e741c002cab215a2cd9e81
lzr97 <55216894+lzr97@users.noreply.github.com>
2020-03-29 13:13:23 -0700

    Add files via upload

---
+++ train_2.py
@@ -0,0 +1,123 @@
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from tensorflow.keras.models import Model
+
+from data import polyvore_dataset, DataGenerator
+from utils_2 import Config
+
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet import MobileNet
+import numpy as np 
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+
+
+if __name__=='__main__':
+
+    # data generators
+    dataset = polyvore_dataset()
+    transforms = dataset.get_data_transforms()
+    X_train, X_test, y_train, y_test, n_classes = dataset.create_dataset()
+
+    if Config['debug']:
+        train_set = (X_train[:100], y_train[:100], transforms['train'])
+        test_set = (X_test[:100], y_test[:100], transforms['test'])
+        dataset_size = {'train': 100, 'test': 100}
+    else:
+        train_set = (X_train, y_train, transforms['train'])
+        test_set = (X_test, y_test, transforms['test'])
+        dataset_size = {'train': len(y_train), 'test': len(y_test)}
+
+    params = {'batch_size': Config['batch_size'],
+              'n_classes': n_classes,
+              'shuffle': True
+              }
+
+    train_generator =  DataGenerator(train_set, dataset_size, params)
+    test_generator = DataGenerator(test_set, dataset_size, params)
+
+
+    # # Use GPU
+    # strategy = tf.distribute.MirroredStrategy()
+    # with strategy.scope():
+    #     base_model = ResNet50(weights='imagenet', include_top=False)
+    #     x = base_model.output
+    #     x = GlobalAveragePooling2D()(x)
+    #     x = Dense(512, activation='relu')(x)
+
+    #     predictions = Dense(n_classes, activation = 'softmax')(x)
+    #     model = Model(inputs=base_model.input, outputs=predictions)
+
+    #     for layer in base_model.layers:
+    #         layer.trainable = False
+
+    #     # define optimizers
+    #     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # model.summary()
+
+    # # training
+    # model.fit_generator(generator=train_generator,
+    #                     validation_data=test_generator,
+    #                     use_multiprocessing=True,
+    #                     workers=Config['num_workers'],
+    #                     )
+
+
+    # Use GPU
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        # base_model = MobileNet(weights='imagenet', include_top=False)
+        # x = base_model.output
+        # x = GlobalAveragePooling2D()(x)
+        # x = Dense(256, activation='relu')(x)
+        # model = keras.models.Sequential()
+        nnet_inputs = keras.layers.Input(shape=(224,224,3))
+        # BLOCK 1
+        x = Conv2D(64, 7, activation='relu')(nnet_inputs)
+        x = MaxPooling2D((3, 3))(x)
+        # BLOCK 2
+        x = Conv2D(64, 7, activation='relu')(x)
+        x = MaxPooling2D((3, 3))(x)
+        # BLOCK 3
+        x = Conv2D(128, 7, activation='relu')(x)
+        x = MaxPooling2D((2, 2))(x)
+        # BLOCK 4
+        x = Conv2D(128, 7, activation='relu')(x)
+        x = MaxPooling2D((2, 2))(x)
+        # x = Flatten()(x)
+        x = Dropout(0.5)(x)
+        # FULLY CONNECTED
+        x = Dense(256, activation='relu')(x)
+        x = Dense(1, activation='sigmoid')(x)
+
+        predictions = Dense(n_classes, activation = 'softmax')(x)
+        model = Model(inputs=nnet_inputs, outputs=predictions)
+
+        # for layer in base_model.layers:
+        #     layer.trainable = False
+
+    # define optimizers
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.summary()
+
+    keras.utils.plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True) 
+
+    # training
+    results = model.fit(train_generator,validation_data=test_generator, epochs=Config['num_epochs'])
+    loss = results.history['loss']
+    val_loss = results.history['val_loss']
+    acc = results.history['accuracy']
+    val_acc = results.history['val_accuracy']
+
+    epochs = np.arange(len(loss))
+
+    plt.figure()
+    plt.plot(epochs, acc, label='acc')
+    plt.plot(epochs, val_acc, label='val_acc')
+    plt.xlabel('epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Learning Curve')
+    plt.legend()
+    plt.savefig('learning_acc_2.png', dpi=256)
---
+++ utils_2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+import os.path as osp
+import argparse
+
+Config ={}
+# you should replace it with your own root_path
+Config['root_path'] = 'polyvore_outfits'
+Config['meta_file'] = 'polyvore_item_metadata.json'
+Config['checkpoint_path'] = ''
+
+
+Config['use_cuda'] = True
+Config['debug'] = False
+Config['num_epochs'] = 20
+Config['batch_size'] = 64
+# Config['debug'] = True
+# Config['num_epochs'] = 2
+# Config['batch_size'] = 128
+Config['learning_rate'] = 0.001
+Config['num_workers'] = 5
